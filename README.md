bbc85974f1cd4ed61edb86ec922963e728e53f52
Kevin Kaske <kevinkaske@gmail.com>
2014-11-27 10:39:06 -0600

    Updating readme

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 readable_date_ranges
 ====================
 
-Add readable range selections to Active Record Models
+Add readable range selections for created_at and modified_at to Active Record Models. These scopes are very helpful for reportings.
+
+## Installation
+
+Add the following to your Gemfile
+```
+gem 'readable_date_ranges'
+```
+
+## Usage
+
+Add the following to any models you would like to add these scopes to
+```
+include ReadableDateRanges
+```
+
+Then you can use the scope in the following way when calling your model
+```
+@users = User.created_this_week
+```
--- lib/readable_date_ranges.rb
+++ lib/readable_date_ranges.rb
@@ -1,18 +1,37 @@
 module readable_date_ranges
 	def self.included(base)
-		base.scope :created_this_week, -> { base.where(created_at: Date.today.at_beginning_of_week.beginning_of_day..Date.today.at_end_of_week.end_of_day) }
-		base.scope :created_last_week, -> { base.where(created_at: 1.week.ago.at_beginning_of_week.beginning_of_day..1.week.ago.at_end_of_week.end_of_day) }
-		base.scope :created_this_month, -> { base.where(created_at: Date.today.at_beginning_of_month.beginning_of_day..Date.today.at_end_of_month.end_of_day) }
-		base.scope :created_last_month, -> { base.where(created_at: 1.month.ago.at_beginning_of_month.beginning_of_day..1.month.ago.at_end_of_month.end_of_day) }
-		base.scope :created_this_year, -> { base.where(created_at: Date.today.at_beginning_of_year.beginning_of_day..Date.today.at_end_of_year.end_of_day) }
-		base.scope :created_last_year, -> { base.where(created_at: 1.year.ago.at_beginning_of_year.beginning_of_day..1.year.ago.at_end_of_year.end_of_day) }
+		this_week_start  = Date.today.at_beginning_of_week.beginning_of_day
+		this_week_end    = Date.today.at_end_of_week.end_of_day
+		last_week_start  = 1.week.ago.at_beginning_of_week.beginning_of_day
+		last_week_end    = 1.week.ago.at_end_of_week.end_of_day
+		this_month_start = Date.today.at_beginning_of_month.beginning_of_day
+		this_month_end   = Date.today.at_end_of_month.end_of_day
+		last_month_start = 1.month.ago.at_beginning_of_month.beginning_of_day
+		last_month_end   = 1.month.ago.at_end_of_month.end_of_day
+		this_year_start  = Date.today.at_beginning_of_year.beginning_of_day
+		this_year_end    = Date.today.at_end_of_year.end_of_day
+		last_year_start  = 1.year.ago.at_beginning_of_year.beginning_of_day
+		last_year_end    = 1.year.ago.at_end_of_year.end_of_day
 		
-		base.scope :modified_this_week, -> { base.where(modified_at: Date.today.at_beginning_of_week.beginning_of_day..Date.today.at_end_of_week.end_of_day) }
-		base.scope :modified_last_week, -> { base.where(modified_at: 1.week.ago.at_beginning_of_week.beginning_of_day..1.week.ago.at_end_of_week.end_of_day) }
-		base.scope :modified_this_month, -> { base.where(modified_at: Date.today.at_beginning_of_month.beginning_of_day..Date.today.at_end_of_month.end_of_day) }
-		base.scope :modified_last_month, -> { base.where(modified_at: 1.month.ago.at_beginning_of_month.beginning_of_day..1.month.ago.at_end_of_month.end_of_day) }
-		base.scope :modified_this_year, -> { base.where(modified_at: Date.today.at_beginning_of_year.beginning_of_day..Date.today.at_end_of_year.end_of_day) }
-		base.scope :modified_last_year, -> { base.where(modified_at: 1.year.ago.at_beginning_of_year.beginning_of_day..1.year.ago.at_end_of_year.end_of_day) }
-
+		base.scope :created_this_week, -> { base.where("created_at between ? AND ?", this_week_start, this_week_end) }
+		base.scope :created_last_week, -> { base.where("created_at between ? AND ?", last_week_start, last_week_end) }
+		base.scope :created_this_month, -> { base.where("created_at between ? AND ?", this_month_start, this_month_end) }
+		base.scope :created_last_month, -> { base.where("created_at between ? AND ?", last_month_start, last_month_end) }
+		base.scope :created_this_year, -> { base.where("created_at between ? AND ?", this_year_start, this_year_end) }
+		base.scope :created_last_year, -> { base.where("created_at between ? AND ?", last_year_start, last_year_end) }
+		
+		base.scope :modified_this_week, -> { base.where("modified_at between ? AND ?", this_week_start, this_week_end) }
+		base.scope :modified_last_week, -> { base.where("modified_at between ? AND ?", last_week_start, last_week_end) }
+		base.scope :modified_this_month, -> { base.where("modified_at between ? AND ?", this_month_start, this_month_end) }
+		base.scope :modified_last_month, -> { base.where("modified_at between ? AND ?", last_month_start, last_month_end) }
+		base.scope :modified_this_year, -> { base.where("modified_at between ? AND ?", this_year_start, this_year_end) }
+		base.scope :modified_last_year, -> { base.where("modified_at between ? AND ?", last_year_start, last_year_end) }
+		
+		base.scope :created_or_modified_this_week, base.where("(created_at between ? AND ?) OR (modified_at between ? AND ?)", this_week_start, this_week_end, this_week_start, this_week_end)}
+		base.scope :created_or_modified_last_week, base.where("(created_at between ? AND ?) OR (modified_at between ? AND ?)", last_week_start, last_week_end, last_week_start, last_week_end)}
+		base.scope :created_or_modified_this_month, base.where("(created_at between ? AND ?) OR (modified_at between ? AND ?)", this_month_start, this_month_end, this_month_start, this_month_end)}
+		base.scope :created_or_modified_last_month, base.where("(created_at between ? AND ?) OR (modified_at between ? AND ?)", last_month_start, last_month_end, last_month_start, last_month_end)}
+		base.scope :created_or_modified_this_year, base.where("(created_at between ? AND ?) OR (modified_at between ? AND ?)", this_year_start, this_year_end, this_year_start, this_year_end)}
+		base.scope :created_or_modified_last_year, base.where("(created_at between ? AND ?) OR (modified_at between ? AND ?)", last_year_start, last_year_end, last_year_start, last_year_end)}
 	end
 end
